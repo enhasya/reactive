d9f25b64c2eb541d55da2033d2297eb99f02ffc4
enhasya <enhasya@outlook.co.id>
2024-02-11 06:33:01 +0700

    update

--- src/pages/auth/createAccount.js
+++ src/pages/auth/createAccount.js
@@ -2,6 +2,9 @@ import React from "react";
 import css from "../../module/ui.module.css";
 
 const {
+  default: ArrowLeft,
+} = require("../../shared/icons/arrow-left-sm-white.svg");
+const {
   default: Username,
 } = require("../../shared/icons/user-profile-03-black.svg");
 const {
@@ -16,7 +19,7 @@ const CreateAccount = () => {
     <main
       className={`${css.auth} flex flex-col lg:flex-row w-full h-auto lg:h-screen items-center justify-center p-8 lg:p-0`}
     >
-      <div className="bg-transparent lg:bg-black/5 flex flex-col w-full lg:w-[360px] h-auto lg:h-[560px] p-0 lg:p-8 ring-0 lg:ring-1 lg:ring-black/10 rounded-none lg:rounded-l-lg">
+      <div className="bg-transparent lg:bg-black/5 backdrop-blur-lg flex flex-col w-full lg:w-[360px] h-auto lg:h-[560px] p-0 lg:p-8 ring-0 lg:ring-1 lg:ring-black/10 rounded-none lg:rounded-l-lg">
         <h1 className="text-blue-600 font-bold text-lg tracking-tight leading-relaxed mt-0">
           Reactive: Revolutionizing Social Networking
         </h1>
@@ -27,9 +30,14 @@ const CreateAccount = () => {
         </h2>
       </div>
       <div className="bg-white flex flex-col items-start justify-between w-full lg:w-[360px] h-auto lg:h-[560px] p-0 lg:p-8 ring-0 lg:ring-1 lg:ring-black/10 rounded-none lg:rounded-r-lg mt-8 lg:mt-0">
-        <p className="text-black font-bold text-lg tracking-tight leading-relaxed mt-0">
-          Reactive Account Creation
-        </p>
+        <div className="flex flex-row gap-4 items-center justify-start w-full">
+          <a href="/" className="bg-black/95 hover:bg-black/75 transition-all p-2 rounded-full">
+            <img src={ArrowLeft} width="20" height="20" alt="ArrowLeft" />
+          </a>
+          <p className="text-black font-bold text-lg tracking-tight leading-relaxed mt-0">
+            Reactive Account Creation
+          </p>
+        </div>
         <form className="flex flex-col w-full">
           <div className="bg-black/5 transition-all flex flex-row gap-4 w-full items-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
             <img src={Username} width="14" height="14" alt="Username" />
@@ -56,15 +64,15 @@ const CreateAccount = () => {
             />
           </div>
           <div className="bg-black/5 transition-all flex flex-row gap-4 w-full items-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
-            <img src={Password} width="14" height="14" alt="Password" />
+            <img src={Password} width="14" height="14" alt="ConfirmPassword" />
             <input
               className="bg-transparent outline-none w-full text-black/80 font-medium text-xs tracking-tight"
               type="password"
               placeholder="Confirm Password"
             />
           </div>
-          <button className="bg-black/95 transition-all flex flex-row gap-4 w-full items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
-            <img src={Authenticate} width="14" height="14" alt="Authenticate" />
+          <button className="bg-black/95 hover:bg-black/75 transition-all flex flex-row gap-4 w-full items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
+            <img src={Authenticate} width="14" height="14" alt="Create" />
             <h2 className="text-white font-bold text-xs tracking-tight leading-relaxed mt-0">
               Create Reactive Account
             </h2>
--- src/pages/auth/signIn.js
+++ src/pages/auth/signIn.js
@@ -16,7 +16,7 @@ const SignIn = () => {
     <main
       className={`${css.auth} flex flex-col lg:flex-row w-full h-auto lg:h-screen items-center justify-center p-8 lg:p-0`}
     >
-      <div className="bg-transparent lg:bg-black/5 flex flex-col w-full lg:w-[360px] h-auto lg:h-[560px] p-0 lg:p-8 ring-0 lg:ring-1 lg:ring-black/10 rounded-none lg:rounded-l-lg">
+      <div className="bg-transparent lg:bg-black/5 backdrop-blur-lg flex flex-col w-full lg:w-[360px] h-auto lg:h-[560px] p-0 lg:p-8 ring-0 lg:ring-1 lg:ring-black/10 rounded-none lg:rounded-l-lg">
         <h1 className="text-blue-600 font-bold text-lg tracking-tight leading-relaxed mt-0">
           Reactive: Revolutionizing Social Networking
         </h1>
@@ -47,7 +47,7 @@ const SignIn = () => {
               placeholder="Password"
             />
           </div>
-          <button className="bg-black/95 transition-all flex flex-row gap-4 w-full items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
+          <button className="bg-black/95 hover:bg-black/75 transition-all flex flex-row gap-4 w-full items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
             <img src={Authenticate} width="14" height="14" alt="Authenticate" />
             <h2 className="text-white font-bold text-xs tracking-tight leading-relaxed mt-0">
               Authenticate to Reactive
@@ -56,9 +56,12 @@ const SignIn = () => {
           <hr width="full" className="mt-8" />
           <p className="text-black font-medium text-xs tracking-tight leading-relaxed mt-8">
             Join Reactive today!{" "}
-            <span className="font-bold text-blue-600 hover:underline">
+            <a
+              href="/auth/create-account"
+              className="font-bold text-blue-600 hover:underline"
+            >
               Create new Account
-            </span>
+            </a>
             . Or if you're having trouble,{" "}
             <span className="font-bold text-blue-600 hover:underline">
               Recover Account
