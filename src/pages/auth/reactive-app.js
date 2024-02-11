606f5f05cbebfb8aa7259119d665e3a38e5fa0cf
enhasya <enhasya@outlook.co.id>
2024-02-11 18:36:23 +0700

    update

--- src/pages/auth/reactive-app.js
+++ src/pages/auth/reactive-app.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+const {
+  default: Authentication,
+} = require("../../shared/icons/shield-check-white.svg");
+
 const ReactiveApp = () => {
   return (
     <div className="bg-transparent lg:bg-black/5 backdrop-blur-lg flex flex-col w-full items-start justify-between lg:w-[360px] h-auto lg:h-[560px] p-0 lg:p-8 ring-0 lg:ring-1 lg:ring-black/10 rounded-none lg:rounded-l-lg">
@@ -14,14 +18,21 @@ const ReactiveApp = () => {
         </h2>
       </div>
       <div className="flex flex-row w-full h-auto gap-4 items-center mt-4 lg:mt-0">
-        <button className="bg-black/95 hover:bg-black/75 transition-all flex flex-row gap-4 w-full lg:w-auto items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-0">
+        <button className="bg-black/95 hover:bg-black/75 transition-all flex flex-row gap-4 w-full items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-0">
+          <img src={Authentication} width="14" height="14" alt="LearnMore" />
           <p className="text-white font-bold text-xs text-start tracking-tight leading-relaxed mt-0">
             Learn More
           </p>
         </button>
-        <button className="bg-black/95 hover:bg-black/75 transition-all flex flex-row gap-4 w-full lg:w-auto items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-0">
+        <button className="bg-black/95 hover:bg-black/75 transition-all flex flex-row gap-4 w-full items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-0">
+          <img
+            src={Authentication}
+            width="14"
+            height="14"
+            alt="Authentication"
+          />
           <p className="text-white font-bold text-xs text-start tracking-tight leading-relaxed mt-0">
-            Data Management
+            Authentication
           </p>
         </button>
       </div>
