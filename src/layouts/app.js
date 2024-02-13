5ee44525c1666929a08f4af92fc3ff42e4567021
enhasya <enhasya@outlook.co.id>
2024-02-13 19:12:47 +0700

    update

--- src/layouts/app.js
+++ src/layouts/app.js
@@ -10,10 +10,10 @@ const LayoutApp = ({ children }) => {
     <main className="flex flex-col w-full h-auto lg:h-screen p-0 mt-0 overflow-y-auto">
       <Navbar />
       <div className="flex flex-col lg:flex-row w-full h-full p-0">
-        <div className="bg-black/5 fixed top-auto left-0 flex flex-col w-full lg:w-[80%] h-full p-8 lg:p-4">
+        <div className="bg-black/5 fixed top-auto left-0 flex flex-col w-full lg:w-[80%] h-full items-start justify-start p-8 lg:p-4">
           {React.Children.map(children, (child) => child)}
         </div>
-        <div className="bg-white fixed top-auto right-0 hidden lg:flex flex-col w-full lg:w-[20%] h-full p-8 lg:p-4">
+        <div className="bg-white fixed top-auto right-0 hidden lg:flex flex-col w-full lg:w-[20%] h-full items-start justify-start p-8 lg:p-4">
           <MessageWidget />
         </div>
       </div>
