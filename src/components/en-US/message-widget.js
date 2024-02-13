4b19458e3d8372399d0a01a775be2d85b228cd7f
enhasya <enhasya@outlook.co.id>
2024-02-13 17:38:43 +0700

    update

--- src/components/en-US/message-widget.js
+++ src/components/en-US/message-widget.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const MessageWidget = () => {
   return (
-    <main className="flex flex-col lg:flex-row gap-2 items-center justify-between w-full h-auto p-8 lg:p-4">
-      Hi
+    <main className="flex flex-col gap-2 items-start justify-center w-full h-auto p-0">
+      <p className="text-black font-bold text-xs text-start tracking-tight leading-relaxed mt-0">
+        Active Chats
+      </p>
+      <p className="text-black font-medium text-xs text-start tracking-tight leading-relaxed mt-0">
+        See the latest messages from your friends
+      </p>
     </main>
   );
 };
