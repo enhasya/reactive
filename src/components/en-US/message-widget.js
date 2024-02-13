0c630fa25ec6e296022f112cd792d2014ebc5002
enhasya <enhasya@outlook.co.id>
2024-02-13 17:36:12 +0700

    update

---
+++ src/components/en-US/message-widget.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageWidget = () => {
+  return (
+    <main className="flex flex-col lg:flex-row gap-2 items-center justify-between w-full h-auto p-8 lg:p-4">
+      Hi
+    </main>
+  );
+};
+
+export default MessageWidget;
--- src/layouts/app.js
+++ src/layouts/app.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 const { default: Navbar } = require("../components/en-US/navbar");
+const {
+  default: MessageWidget,
+} = require("../components/en-US/message-widget");
 
 const LayoutApp = ({ children }) => {
   return (
     <main className="flex flex-col w-full h-auto lg:h-screen p-0 mt-0">
       <Navbar />
-      <div className="bg-black/5 flex w-full h-full p-8 lg:p-4">
-        {React.Children.map(children, (child) => child)}
+      <div className="flex flex-col lg:flex-row w-full h-full p-0">
+        <div className="bg-black/5 flex flex-col w-full lg:w-[80%] h-full p-8 lg:p-4">
+          {React.Children.map(children, (child) => child)}
+        </div>
+        <div className="bg-white flex flex-col w-full lg:w-[20%] h-full p-8 lg:p-4">
+          <MessageWidget />
+        </div>
       </div>
     </main>
   );
