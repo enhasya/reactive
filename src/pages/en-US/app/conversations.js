3a3976491d5bd8385daedc46d240c32beda0f52b
enhasya <enhasya@outlook.co.id>
2024-02-18 18:20:40 +0700

    update

--- src/pages/en-US/app/conversations.js
+++ src/pages/en-US/app/conversations.js
@@ -3,16 +3,19 @@ import React from "react";
 const { default: LayoutApp } = require("../../../layouts/app");
 const {
   default: Type,
-} = require("../../../shared/icons/user-profile-03-black.svg");
+} = require("../../../shared/icons/message-text-01-black.svg");
+const {
+  default: Send,
+} = require("../../../shared/icons/send-01-white.svg");
 
 const Conversations = () => {
   return (
     <LayoutApp>
-      <div className="bg-transparent flex flex-col w-full h-auto lg:h-full items-center justify-between gap-0 p-4 border-y border-r border-y-black/10 border-r-black/10 rounded-none lg:rounded-r-lg overflow-y-auto">
-        <p className="text-black font-bold text-base text-start tracking-tight leading-relaxed w-full mt-0">
+      <div className="bg-transparent flex flex-col w-full h-auto lg:h-full items-center justify-between gap-0 p-4 overflow-y-auto">
+        <p className="text-black font-bold text-base text-start tracking-tight leading-relaxed w-full mt-0 mb-4">
           [parameter:roomName]
         </p>
-        <div className="flex flex-col w-full h-full p-0 mt-0">
+        <div className="flex flex-col w-full h-full p-0 mt-0 overflow-y-auto">
           <div className="flex w-full items-center justify-start">
             <div className="flex flex-col w-[320px] h-auto gap-2 mt-4">
               <p className="text-black font-bold text-xs text-start tracking-tight leading-relaxed mt-0">
@@ -116,7 +119,7 @@ const Conversations = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col z-10 w-full h-auto p-0 mt-0">
+        <div className="flex flex-row w-full h-auto gap-4 p-0 mt-0">
           <div className="bg-black/5 transition-all flex flex-row gap-4 w-full items-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
             <img src={Type} width="14" height="14" alt="Type" />
             <input
@@ -125,6 +128,12 @@ const Conversations = () => {
               placeholder="Type something ..."
             />
           </div>
+          <button className="bg-black transition-all flex flex-row gap-4 w-[20%] hover:w-[25%] items-center justify-center p-4 rounded-lg ring-1 ring-black/10 hover:ring-black/20 mt-4">
+            <p className="text-white font-bold text-xs tracking-tight leading-relaxed mt-0">
+              Send
+            </p>
+            <img src={Send} width="14" height="14" alt="Send" />
+          </button>
         </div>
       </div>
     </LayoutApp>
